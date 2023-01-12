8d2c36fab267961272ef467836544101e0088cfb
Uexcel2022 <121309125+Uexcel2022@users.noreply.github.com>
2023-01-12 02:04:33 +0100

    edit

--- EmailRegex.py
+++ EmailRegex.py
@@ -4,8 +4,7 @@ def EmailRegex(Email):
     import time
     import re
     time.sleep(0.2)
-    if check := re.fullmatch("[a-z0-9]*[._]?[a-z0-9]+[_.]?[a-z0-9]+[@][a-z]+[.]?[a-z]*[.][a-z]+", email):
+    if check := re.fullmatch("[a-z0-9]*[._]?[a-z0-9]+[_.]?[a-z0-9]+@[a-z0-9]+[.]?[a-z]*[.][a-z]+", email):
         return check.group()
 
-
-#EmailRegex("k.ez_902@gmail.com")
+# print(EmailRegex("mala@cs50.harvard.edu"))
