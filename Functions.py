9c67e325812ff29e19254ff0a8df0b1ebb06a98b
Uexcel2022 <121309125+Uexcel2022@users.noreply.github.com>
2023-01-16 17:35:38 +0100

    edited

--- Functions.py
+++ Functions.py
@@ -72,11 +72,12 @@ def GenerateCode():
     from string import printable
     from random import randint
     pin = str(randint(1000, 9999))
-    userPrbKey = ''
+    
+    userPrKey = ''
     for i in range(7):
         rand = randint(10, 61)
         key = printable[rand]
-        userPrbKey += key
+        userPrKey += key
 
     userPubKey = ''
     for i in range(10):
@@ -84,7 +85,7 @@ def GenerateCode():
         key = printable[rand]
         userPubKey += key
 
-    return pin, userPrbKey, userPubKey
+    return pin, userPrKey, userPubKey
 
 def Registration():
     import time
